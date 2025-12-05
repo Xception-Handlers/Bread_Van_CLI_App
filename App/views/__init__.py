e4c95acb02c1a9483b8db7b897f6a4658fa0b216
sky <skylamraj.028@gmail.com>
2025-12-04 22:28:35 -0400

    updated views init

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -8,7 +8,7 @@ from .driver_views import driver_views
 from .resident_views import resident_views
 from .admin_views import admin_views
 from .common_views import common_views
+from .web import web_views
 
-
-views = [user_views, index_views, auth_views, common_views, driver_views, resident_views, admin_views]
+views = [user_views, index_views, auth_views, common_views, driver_views, resident_views, admin_views, web_views]
 # blueprints must be added to this list
